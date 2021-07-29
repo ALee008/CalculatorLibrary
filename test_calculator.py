9689483f717841a3af3db2e8526e89904b31348a
ALee008 <NRW6073@nrwbanki.de>
2021-07-29 14:50:09 +0200

    added division to calculator

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,10 @@
 """
 Unit tests for the calculator library
 """
+import math
+
+import pytest
+
 import calculator
 
 
@@ -11,6 +15,13 @@ class TestCalculator:
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
+        assert math.isclose(1 - 0.99, 0.01)
 
     def test_multiplication(self):
         assert 100 == calculator.multiply(10, 10)
+
+    def test_division(self):
+        assert 2 == calculator.divide(8, 4)
+        assert calculator.divide(4.5, 2.25) == 2.0
+        with pytest.raises(ZeroDivisionError):
+            calculator.divide(1, 0)
